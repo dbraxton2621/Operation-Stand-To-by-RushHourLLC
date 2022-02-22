7788632b167e69143c79279ae4ebb2fd7e976cc9
Jimmy Vu <jimmy.h.vu_23@outlook.com>
2022-02-22 15:26:47 -0800

    some ideas for the StandToApp

--- src/com/RushHour/StandToApp.java
+++ src/com/RushHour/StandToApp.java
@@ -3,10 +3,35 @@ package com.RushHour;
 public class StandToApp {
 
     public void execute() {
-        enemy1();
+        Player player = new Player(100, 1);
+        Enemy enemy = new Enemy(100, 1);
+        boolean game = false;
+        Battlefield battlefield = new Battlefield(player, enemy, game);
 
+        play(battlefield);
+
+        if(player.getHealth() > 0 ){
+
+            enemy.setSoldiers(120);
+            enemy.setHealth(120);
+            player.setSoldiers(100);
+            player.setHealth(100);
+            game = false;
+
+            battlefield = new Battlefield(player, enemy, game);
+
+            play(battlefield);
+        }
+        System.out.println("done");
+    }
+    public void play(Battlefield battlefield){
+        battlefield.initializeBoard();
+        while (!battlefield.isGameEnd()) {
+            battlefield.menu();
+        }
     }
 
+    /*
     public void enemy1() {
         Player player = new Player(100, 100);
         Enemy enemy = new Enemy(100, 100);
@@ -19,4 +44,6 @@ public class StandToApp {
         }
         System.out.println("done");
     }
+
+     */
 }
