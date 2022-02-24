592acd15b1d57a6270da4c1fdbb779767b684a84
Jimmy Vu <jimmy.h.vu_23@outlook.com>
2022-02-24 10:05:27 -0800

    Added build.cmd and build.sh. reconfigured run.sh

--- build.cmd
+++ build.cmd
@@ -1,5 +1,5 @@
 @echo off
 
-javac -d classes -classpath "lib\*" src\com\RushHour\*.java src\com\RushHour\client\*.java
+javac -d classes -classpath "lib\*" src\com\RushHour\client\*.java src\com\RushHour\*.java
 
 jar --create --file standTo-1.0.jar -C classes .
--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env sh
 
-javac -d classes -classpath "lib/*" src/com/RushHour/*.java src/com/RushHour/client/*.java
+javac -d classes -classpath "lib/*" src/com/RushHour/client/*.java src/com/RushHour/*.java
 
 
 jar --create --file standTo-1.0.jar -C classes .
